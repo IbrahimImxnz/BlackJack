c0d20afb6207cb57df7ca2c76a430ed5bf058214
Ibrahim Maaly <Jokerimxnz@gmail.com>
2024-05-02 01:25:07 +0200

    third commit

--- BlackJack.py
+++ BlackJack.py
@@ -1,6 +1,6 @@
 import random
 values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8, 
-            'Nine':9, 'Ten':10, 'Jack':11, 'Queen':12, 'King':13, 'Ace':14}
+            'Nine':9, 'Ten':10, 'Jack':10, 'Queen':10, 'King':10, 'Ace':11, "Acee":1}
 suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
 ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
 
@@ -37,4 +37,26 @@ class Player:
     def __init__(self, balance, name):
         self.balance = balance
         self.name = name
+        self.cards = []
+
+    def hit(self,card):
+        self.cards.append(card)
+
+    def Stay(self):
+        pass
+
+    def withdraw(self,amount):
+        if self.balance >= amount:
+            self.balance -= amount
+            print("Withdrawal accepted")
+        else:
+            return False
+
+    def deposit(self,amount):
+        self.balance += amount
+        print("Money deposited")        
+
+
+      
+
 
