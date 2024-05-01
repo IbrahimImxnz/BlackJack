89becd35ecb2853222b2f50ad0e22e14330b9492
Ibrahim Maaly <Jokerimxnz@gmail.com>
2024-05-02 01:48:05 +0200

    game prep

--- BlackJack.py
+++ BlackJack.py
@@ -1,6 +1,6 @@
 import random
 values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8, 
-            'Nine':9, 'Ten':10, 'Jack':10, 'Queen':10, 'King':10, 'Ace':11, "Acee":1}
+            'Nine':9, 'Ten':10, 'Jack':10, 'Queen':10, 'King':10, 'Ace':[1,11]}
 suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
 ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
 
@@ -34,9 +34,8 @@ class Deck:
     
 class Player:
 
-    def __init__(self, balance, name):
+    def __init__(self, balance):
         self.balance = balance
-        self.name = name
         self.cards = []
 
     def hit(self,card):
@@ -56,7 +55,38 @@ class Player:
         self.balance += amount
         print("Money deposited")        
 
+game_on = True
 
-      
+while game_on: 
+    deck = Deck()
+    balance = int(input("How much is your balance?"))
+    player = Player(balance)
+    dealer = Player(0)
+
+    bet = int(input("How much would you like to bet?"))
+
+    betting_amount = player.withdraw(bet)
+
+    for i in range(2):
+        player.hit(deck.deal_one())
+        dealer.hit(deck.deal_one())
+        
+    if player.cards[0] + player.cards[1] == 21 and dealer.cards[0] + dealer.cards[1] == 21:
+        print("Push! You did not lose your bet, but you did not win the game!")
+        player.deposit(betting_amount)
+        choices = True
+        while choices:
+            choice = input("play again or quit?")
+            if choice == "play again":
+                choices = False
+                continue
+            elif choice == "quit":
+                choices = False
+                game_on = False
+                break
+            else:
+                print("please type either play again or quit")
+
+
 
 
